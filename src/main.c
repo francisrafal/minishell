e4bd40af0fc256e913df85b45a86b650084773bd
Francis Rafal <francis.rafal@gmail.com>
2023-02-06 10:12:22 +0100

    feat: Give Back Prompt After Command Execution

--- src/main.c
+++ src/main.c
@@ -5,12 +5,17 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	char	*cmd_line = get_cmd_line();
-	// Test echo
-	char	**echo_test_cmd = ft_split(cmd_line, ' ');
-	echo(echo_test_cmd);
+	char	*cmd_line;
+	char	**echo_test_cmd; 
 
-	free(cmd_line);
-	// echo_test_cmd must be freed too
+	while (1)
+	{		
+		cmd_line = get_cmd_line();
+		// Test echo
+		echo_test_cmd = ft_split(cmd_line, ' ');
+		echo(echo_test_cmd);
+		free(cmd_line);
+		free_arr(echo_test_cmd);	
+	}
 	return (0);
 }
