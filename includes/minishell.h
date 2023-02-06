cbe79f2ddff180c7c22b1e7e0d0be8c3965421e7
celgert <celgert@student.42vienna.com>
2023-02-06 08:14:54 +0100

    feat: structs for commands

--- includes/minishell.h
+++ includes/minishell.h
@@ -9,4 +9,29 @@
 /* Project Headers */
 # include "libft.h"
 
+/*struct for list of cmds*/
+typedef struct s_lst_cmds
+{
+	char	**path;
+	char	**opt;
+	int		fd;
+	struct s_lst_cmds	*next;
+}		t_cmds;
+
+/*struct for new command from minishell*/
+typedef struct s_cmd
+{
+	int			re_in;
+	int			re_out;
+	int			read_in;
+	char		*delim;
+	int			re_out_app;
+	t_cmds		lst_cmds;
+}		t_cmd;
+
+/*commands in list_utils*/
+t_cmds	*ft_lstnew(int fd);
+void	ft_lstadd_back(t_cmds **lst, t_cmds *new);
+//void	ft_display_lst(t_cmds *lst);
+void	free_lst(t_cmds **lst);
 #endif
