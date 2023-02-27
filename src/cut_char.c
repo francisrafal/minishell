f066c61d8a8493fa7b9619759de85ac81a998a0a
Christiane Elgert <christiane.elgert@univie.ac.at>
2023-02-27 15:46:30 +0100

    fix char **env

--- src/cut_char.c
+++ src/cut_char.c
@@ -1,32 +1,39 @@
 #include "minishell.h"
 
-int	check_str_env(char *str, t_env env, int k)
+int	check_str_env(char *str, t_env *env, int k)
 {
+	t_env_node      *tmp;
 	char	*new;
-	char	*tmp;
-	int		i;
 
 	new = (char *)malloc(sizeof(char) * (k + 1));
 	if (!new)
 		return (0);
 	ft_strlcpy(new, str, k + 1);
-	tmp = ft_strjoin(new, "=");
+	tmp = find_env_node(env, new);
 	free(new);
-	if (!tmp)
-		return (0);
-	i = 0;
-	// CORRECT ENVP HERE:
-	while (envp[i] && ft_strncmp(tmp, envp[i], k+1))
-		i++;
-	if (!(envp[i]))
-		return (0);
-	free(tmp);
-	return ((int)ft_strlen(&envp[i][k + 1]));
+        if (tmp == NULL)
+               	return (0);
+        else
+                return ((int)ft_strlen(tmp->value));
 }
 
-char	*get_str_env(char *str, char **envp, int k)
+char	*get_str_env(char *str, t_env *env, int k)
 {
-	char	*new;
+	t_env_node      *tmp;
+        char    *new;
+
+        new = (char *)malloc(sizeof(char) * (k + 1));
+        if (!new)
+                return (0);
+        ft_strlcpy(new, str, k + 1);
+        tmp = find_env_node(env, new);
+        free(new);
+        if (tmp == NULL)
+                return (0);
+        else
+                return (tmp->value);
+}
+/*	char	*new;
     char	*tmp;
 	int		i;
 
@@ -49,8 +56,9 @@ char	*get_str_env(char *str, char **envp, int k)
 		return (0);
 	ft_strlcpy(new, &envp[i][k + 1], (int)ft_strlen(&envp[i][k + 1]) + 1);
 	return(new);
-}
-int	count_chars(char *str, char **envp)
+}*/
+
+int	count_chars(char *str, t_env *env)
 {
 	int	i;
 	int	j;
@@ -71,7 +79,7 @@ int	count_chars(char *str, char **envp)
 				k = get_next_char(&str[i+j+1], "\t \"\'");
 				if (k != 0)
 				{
-					l =  check_str_env(&str[i+j+1],envp ,k);
+					l =  check_str_env(&str[i+j+1],env ,k);
 					count += k + 1 - l;
 				}
 				j += get_next_char(&str[i + j + k + 1], "$")+ k + 1;
@@ -88,7 +96,7 @@ int	count_chars(char *str, char **envp)
 			k = get_next_char(&str[i+1], "\t \"\'");
                         if (k != 0)
                         {
-				l =  check_str_env(&str[i+j+1],envp ,k);
+				l =  check_str_env(&str[i+j+1],env ,k);
                                 count += k + 1 - l;
 			}
 			i += k + 1;
@@ -99,7 +107,7 @@ int	count_chars(char *str, char **envp)
 	return (count);
 }
 
-char	*replace_chars(char *str, char **envp)
+char	*replace_chars(char *str, t_env *env)
 {
 	char 	*line;
 	int	size;
@@ -112,7 +120,8 @@ char	*replace_chars(char *str, char **envp)
 	char 	*tmp;
 	
 	//init_idx(i,5);
-	size = (int)ft_strlen(str) - count_chars(str, envp);
+	size = (int)ft_strlen(str) - count_chars(str, env);
+	printf("works");
 	line = (char *)malloc(sizeof(char) *(size +1));
 	if (!line)
 		return (NULL);
@@ -136,10 +145,10 @@ char	*replace_chars(char *str, char **envp)
 				if (k != 0)
 				{
 					//printf("%s\n %s\n", &str[i], line);
-					l =  check_str_env(&str[i+1],envp ,k);
+					l =  check_str_env(&str[i+1],env ,k);
 					if(l)
 					{	
-						tmp = get_str_env(&str[i+1], envp, k);
+						tmp = get_str_env(&str[i+1], env, k);
                                                 ft_strlcpy(&line[m], tmp, l+1);
                                                	m += l;
                                                 i += k;
@@ -168,14 +177,14 @@ char	*replace_chars(char *str, char **envp)
                         if (k != 0)
                         {
 				//printf("%s\n %s\n", &str[i+1], line);
-				l =  check_str_env(&str[i+1],envp ,k);
+				l =  check_str_env(&str[i+1],env ,k);
 				if(l)
 				{
-					tmp = get_str_env(&str[i+1], envp, k);
+					tmp = get_str_env(&str[i+1], env, k);
 					ft_strlcpy(&line[m], tmp, l+1);
 					m += l;
 					i += k + 1;
-                                        free(tmp);
+                                       // free(tmp);
 				}
 				else
 					i += k + 1;
--- src/get_command.c
+++ src/get_command.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-char	*replace_vars(char *cmd, char **envp)
+/*char	*replace_vars(char *cmd, char **envp)
 {
 	char	*trim;
 	(void)envp;
@@ -21,78 +21,6 @@ char	*replace_vars(char *cmd, char **envp)
 	return (trim);
 }
 
-
-/*char	*cutted_spaces(char *str, char *new, char c)
-{
-	int	i; 
-	int	j;
-	int	k;
-	
-	i = 0;
-	j = 1;
-	k = 0;
-	while (str[i])
-	{
-		new[k] = str[i];
-		if (str[i] == c)
-		{
-			while (str[i+j] && str[i+j] == c)
-				j++;
-		}
-		i += j;
-		j=1;
-		k++;
-	}
-	new[k] = '\0';
-	return (new);
-}
-
-int     n_add_char(char *str, char c)
-{
-        int     i;
-        int     j;
-        int     k;
-
-        i = 0;
-        j = 1;
-        k = 0;
-        while (str[i])
-        {
-                if (str[i] == c)
-                {
-                        while (str[i+j] && str[i+j] == c)
-                        {
-                                j++;
-                                k++;
-                        }
-                }
-                i+= j+1;
-                j=1;
-        }
-        return (k);
-}
-
-char *rm_space(char *str)
-{
-	int	len;
-	int	nspace;
-	char	*new;
-
-	len = (int)ft_strlen(str);
-	nspace = n_add_char(str, ' ');
-	if (nspace == 0)
-		return (str);
-	new = (char *)malloc(sizeof(char) * (len - nspace + 1));
-	if (!new)
-	{
-		free(str);
-		return (NULL);
-	}
-	new = cutted_spaces(str, new, ' ');
-	free(str); 
-	printf(" len %i | add space %i\n", len, nspace);
-	return (new);
-}*/
 int	is_char(char *str, char c)
 {
 	int	i;
@@ -112,25 +40,28 @@ int	is_char(char *str, char c)
 		i++;
 	}
 	return (0);
-}
+}*/
 
 char	*get_command(t_cmd *cmd, char *cmd_str, t_env *env)
 {
 	int	i;
 	int	nopt;
 	char	*str;
+	char	*tmp;
 
-	str = prun_str(cmd_str);
+//	str = prun_str(cmd_str);
+	tmp = replace_chars(cmd_str, env);
+	str = prun_str(tmp);
 	cmd->opt = split_char(str, &nopt, ' ');
 	if (!cmd->opt)
 		return (NULL);
 	i = 0;
 	while (i < nopt)
 	{
-		if (is_char(cmd->opt[i], '\'') || is_char(cmd->opt[i], '"'))
-		{
-			cmd->opt[i] = replace_chars(cmd->opt[i], env);
-		}
+		//if (is_char(cmd->opt[i], '\'') || is_char(cmd->opt[i], '"'))
+		//{
+		//cmd->opt[i] = replace_chars(cmd->opt[i], env);
+		//}
 		i++;
 	}
 	return (str);
