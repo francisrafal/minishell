993c8a5b56904cc2e1b4bbb1e9d300882584dded
celgert <celgert@student.42vienna.com>
2023-03-03 15:01:41 +0100

    fix norm: list_utils.c

--- src/list_utils.c
+++ src/list_utils.c
@@ -5,20 +5,18 @@ t_cmd	*ft_lstnew(int ncmds)
 	t_cmd	*elem;
 
 	elem = (t_cmd *)malloc(sizeof(t_cmd));
-	if (elem)
-	{	
-		elem->re_in = 0;
-		elem->re_out = 0;
-		elem->read_in = 0;
-		elem->delim = NULL;
-		elem->fd_in = 0;
-		elem->fd_out = 1;
-		elem->ncmds = ncmds;
-		elem->next = NULL;
-		elem->path = NULL;
-		elem->opt = NULL;
-		//elem->fd_in = fd;
-	}
+	if (!elem)
+		return (NULL);
+	elem->re_in = 0;
+	elem->re_out = 0;
+	elem->read_in = 0;
+	elem->delim = NULL;
+	elem->fd_in = 0;
+	elem->fd_out = 1;
+	elem->ncmds = ncmds;
+	elem->next = NULL;
+	elem->path = NULL;
+	elem->opt = NULL;
 	return (elem);
 }
 
@@ -44,7 +42,7 @@ void	*free_lst_null(t_cmd *node)
 	if (node == NULL)
 		return (NULL);
 	while (node)
-        {
+	{
 		node->delim = free_null(node->delim);
 		node->path = free_arr_null(node->path);
 		node->opt = free_arr_null(node->opt);
@@ -54,8 +52,7 @@ void	*free_lst_null(t_cmd *node)
 	}
 	return (NULL);
 }
-
-
+/*
 void	ft_display_lst(t_cmd *lst)
 {
 	int	i;
@@ -93,4 +90,4 @@ void	ft_display_lst(t_cmd *lst)
 		lst = lst->next;
 		j++;
 	}
-}
+}*/
