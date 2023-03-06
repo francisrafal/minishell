ba6b34786be1f0e17867a629459480b5744c3c1d
Francis Rafal <francis.rafal@gmail.com>
2023-03-06 13:22:26 +0100

    refactor: minishell_tester.py

--- minishell_tester.py
+++ minishell_tester.py
@@ -1,19 +1,37 @@
 import pexpect
 
+testcmds = ['echo hello', 'exit']
+minishell = pexpect.spawn('/home/frafal/minishell/minishell')
+bash = pexpect.spawn('bash')
+
 def result(str):
-    if (child.expect(str) == 0):
+    if (minishell.expect(str) == 0):
         return 'OK'
     return 'KO'
 
-child = pexpect.spawn('/home/frafal/minishell/minishell')
-print('\nTest 01')
-print('Command:\t\techo hello')
-child.sendline('echo hello')
-print('Expected:\t\thello')
-print(result('hello'))
+def test(cmd, testnum):
+    print(f"\nTest {testnum:03d}")
+    print(f"{'Command:':10}{cmd}")
+    minishell.sendline(cmd)
+    # bash.sendline(cmd)
+    # print(f"{'Expected:':10}{bash.read}")
 
-print('\nTest 02')
-print('Command:\t\texit')
-child.sendline('exit')
-print('Expected:\t\texit')
-print(result('exit'))
+
+def main():
+    print('\nTest 01')
+    print('Command:\t\techo hello')
+    minishell.sendline('echo hello')
+    print('Expected:\t\thello')
+    print(result('hello'))
+
+    print('\nTest 02')
+    print('Command:\t\texit')
+    minishell.sendline('exit')
+    print('Expected:\t\texit')
+    print(result('exit'))
+
+    for testnum, cmd in enumerate(testcmds):
+        test(cmd, testnum)
+
+if __name__ == "__main__":
+    main()
