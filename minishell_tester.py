f13b2564614fb17d51ccca55f49d598e5223d7bb
Francis Rafal <francis.rafal@gmail.com>
2023-03-06 14:45:08 +0100

    feat: Print OK and KO In tests

--- minishell_tester.py
+++ minishell_tester.py
@@ -1,35 +1,43 @@
 import pexpect
+import sys
+import os
 
-testcmds = ['echo hello', 'exit']
-minishell = pexpect.spawn('/home/frafal/minishell/minishell')
-bash = pexpect.spawn('bash')
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-def result(str):
-    if (minishell.expect(str) == 0):
-        return 'OK'
-    return 'KO'
+testcmds = [["echo hello", "hello"],
+            ["exit", "exit"],
+            ["cat nonexistingfile", "cat: nonexistingfile: No such file or directory"]]
+
+def result(minishell, str):
+    try:
+        minishell.expect("\r\n" + str + "\r\n", timeout=1)
+        return bcolors.OKGREEN + "OK" + bcolors.ENDC
+    except:
+        return bcolors.FAIL + "KO" + bcolors.ENDC
 
 def test(cmd, testnum):
-    print(f"\nTest {testnum:03d}")
-    print(f"{'Command:':10}{cmd}")
-    minishell.sendline(cmd)
-    # bash.sendline(cmd)
-    # print(f"{'Expected:':10}{bash.read}")
+    minishell = pexpect.spawn(sys.argv[1])
+    logfile = "testlogs/minishell_tester_" + str(testnum).zfill(3) + ".log"
+    os.makedirs(os.path.dirname(logfile), exist_ok=True)
+    minishell.logfile = open(logfile, "wb")
+    print(bcolors.HEADER + bcolors.BOLD + f"\nTest {testnum:03d}" + bcolors.ENDC)
+    print(f"{'Command:':10}{cmd[0]}")
+    minishell.sendline(cmd[0])
+    print(f"{'Expected:':10}{cmd[1]}")
+    print(result(minishell, cmd[1]))
+    minishell.logfile.close()
 
 
 def main():
-    print('\nTest 01')
-    print('Command:\t\techo hello')
-    minishell.sendline('echo hello')
-    print('Expected:\t\thello')
-    print(result('hello'))
-
-    print('\nTest 02')
-    print('Command:\t\texit')
-    minishell.sendline('exit')
-    print('Expected:\t\texit')
-    print(result('exit'))
-
     for testnum, cmd in enumerate(testcmds):
         test(cmd, testnum)
 
